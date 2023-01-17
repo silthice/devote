be5bc5f47ad4a157ca5b9e2019421ad5d593f885
silthice <ryonlim@hotmail.com>
2023-01-17 18:34:24 +0800

    - Added widget for Devote

---
+++ DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by Giap on 17/01/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+    }
+}
